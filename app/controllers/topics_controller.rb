f080bc245e4c61bd846e9c5872d1e221a1f67e7a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-05-05 15:31:09 +0000

    トピックの追加機能と削除機能を実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,20 @@
 class TopicsController < ApplicationController
+
   def index
     @topics = Topic.all
+    @newTopic = Topic.new
   end
+
+  def create
+    @topic = Topic.new(params[:topic].permit(:title))
+    @topic.save
+    redirect_to topics_index_path
+  end
+
+  def delete
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_index_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'topics/index'
   get 'topics/show/:id' => 'topics#show', as: :topics_show
+  delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
+  post 'topics/create' => 'topics#create'
 end
